95ee4af469a3def171367f6b90d04407d90b839f
xiaohua <3103506013@qq.com>
2021-11-10 23:04:26 +0800

    11.10

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 import { getDate, getTime, formatSeconds } from './modules/date-and-time.js'
+import { timerForward, timerBackward, timerOver, setPercent } from './modules/timer.js'
+import { addOneItem } from './modules/table.js'
 
-let time = document.getElementsByClassName('time')[0]
 let radioTimerForward = document.getElementById('forward')
 let radioTimerBackward = document.getElementById('backward')
 let start = document.getElementById('start')
 let end = document.getElementById('end')
-let timeTable = document.getElementById('time-table')
 let timerInput = document.getElementById('time-value')
-const circleDom = document.getElementsByClassName('circle')[0]
-const textDom = document.getElementsByClassName('time')[0]
+
 
 let isRunning = false
-let intervalTimer = 0
+
 let today = getDate()
 const storage = JSON.parse(localStorage.getItem(today)) || []
 storage.forEach((item) => {
-    timeTable.appendChild(getNode(item))
+    addOneItem(item)
 })
 // 创建构造函数
-function TimeItem(startTime, endTime) {
-    this.startTime = startTime
-    this.endTime = endTime
+function TimeItem() {
+    this.startTime = getTime()
+    this.endTime = ''
     this._resultTime = 0
     Object.defineProperty(this, "resultTime", {
         get: function() {
@@ -37,46 +36,19 @@ function TimeItem(startTime, endTime) {
 start.onclick = function() {
     if (!isRunning) {
         let item = new TimeItem()
+
         isRunning = true
         start.classList.remove('clickable')
         end.classList.add('clickable')
         let ifForward = document.getElementsByName("timer")[0].checked
-        ifForward ? timerForward(item) : timerBackward(item)
+        ifForward ? timerForward() : timerBackward(parseInt(timerInput.value) * 60)
         storage.push(item)
     } else {
         console.log('正在运行中...')
     }
 }
 
-
-function timerForward(item) {
-
-    item.startTime = getTime()
-
-    let total = 0
-    time.innerHTML = '00:00:00'
-    intervalTimer = setInterval(() => {
-        total++
-        time.innerHTML = formatSeconds(total)
-    }, 1000)
-
-}
-
-function timerBackward(item) {
-    item.startTime = getTime()
-
-    let total = parseInt(timerInput.value) * 60 // 以秒为单位
-    let current = total
-    time.innerHTML = formatSeconds(total)
-    intervalTimer = setInterval(() => {
-        current--
-        current == 0 ? timeOver() : ''
-        time.innerHTML = formatSeconds(current)
-        setPercent(current, total)
-    }, 1000)
-}
-
-function timeOver() {
+end.onclick = function() {
     if (isRunning) {
         isRunning = false
         end.classList.remove('clickable')
@@ -84,26 +56,12 @@ function timeOver() {
     
         let item = storage[storage.length - 1]
         item.endTime = getTime()
-    
-        clearInterval(intervalTimer)
-        timeTable.appendChild(getNode(item))
+        timerOver()
+        addOneItem(item)
         localStorage.setItem(today, JSON.stringify(storage))
     } else {
         console.log('尚未运行')
     }
-}
-
-end.onclick = timeOver
-
-function setPercent (current = 1, total = 1) {
-    const GRITH = 618
-    circleDom.style['stroke-dashoffset'] = GRITH * (current / total)
-}
-
-function getNode(item) {
-    let li = document.createElement('li')
-    li.innerHTML = `<li>${item.startTime} - ${item.endTime} 总计：${item.resultTime || item._resultTime} min</li>`
-    return li
 }
 
 radioTimerForward.onclick = function() {
